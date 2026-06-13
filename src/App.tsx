eeb333c216f327761937943d6140986584015486
Sabyrzhan Zhaslanuly <sabyrka.me@gmail.com>
2026-06-13 23:02:45 +0500

    feat: connect categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Header } from './components/Header'
+import { Categories } from './components/Categories'
 import { Main } from './components/Main'
 import { Footer } from './components/Footer'
 
@@ -6,6 +7,7 @@ export const App = () => {
   return (
     <>
       <Header />
+      <Categories />
       <Main />
       <Footer />
     </>
