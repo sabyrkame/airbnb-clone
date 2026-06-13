bd1ffe0149f4ab4726bddffda159154bdc29d736
Sabyrzhan Zhaslanuly <sabyrka.me@gmail.com>
2026-06-13 22:01:47 +0500

    refactor: add header inner tag

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,56 +8,58 @@ import './Header.scss'
 
 export const Header = () => {
   return (
-    <header className="header container">
-      <a className="header__logo" href="/">
-        <img
-          className="header__logo-image"
-          src={logoSvg}
-          alt="airbnb logo"
-          width={102}
-          height={32}
-        />
-      </a>
-      <nav className="header__menu">
-        <ul className="header__menu-list">
-          {menuItems.map(({ label, href, hasIcon }, index) => (
-            <li className="header__menu-item" key={index}>
-              <a className="header__menu-link" href={href}>
-                {label}
-              </a>
-              {hasIcon && (
-                <button className="header__menu-search-button" type="button">
-                  <img src={searchIconSvg} alt="" width={12} height={12} />
-                </button>
-              )}
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <div className="header__actions">
-        <button className="header__button" type="button">
-          Airbnb your home
-        </button>
-        <button className="header__lang-button">
-          <img src={globeIconSvg} alt="" width={16} height={16} />
-        </button>
-        <div className="header__profile">
-          <button className="header__profile-nav">
-            <img
-              className="header__profile-nav-image"
-              src={burgerButtonSvg}
-              alt=""
-              width={16}
-              height={16}
-            />
-          </button>
+    <header className="header ">
+      <div className="header__inner container">
+        <a className="header__logo" href="/">
           <img
-            className="header__profile-avatar"
-            src={profileImage}
-            alt=""
-            width={30}
-            height={30}
+            className="header__logo-image"
+            src={logoSvg}
+            alt="airbnb logo"
+            width={102}
+            height={32}
           />
+        </a>
+        <nav className="header__menu">
+          <ul className="header__menu-list">
+            {menuItems.map(({ label, href, hasIcon }, index) => (
+              <li className="header__menu-item" key={index}>
+                <a className="header__menu-link" href={href}>
+                  {label}
+                </a>
+                {hasIcon && (
+                  <button className="header__menu-search-button" type="button">
+                    <img src={searchIconSvg} alt="" width={12} height={12} />
+                  </button>
+                )}
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className="header__actions">
+          <button className="header__button" type="button">
+            Airbnb your home
+          </button>
+          <button className="header__lang-button">
+            <img src={globeIconSvg} alt="" width={16} height={16} />
+          </button>
+          <div className="header__profile">
+            <button className="header__profile-nav">
+              <img
+                className="header__profile-nav-image"
+                src={burgerButtonSvg}
+                alt=""
+                width={16}
+                height={16}
+              />
+            </button>
+            <img
+              className="header__profile-avatar"
+              src={profileImage}
+              alt=""
+              width={30}
+              height={30}
+            />
+          </div>
         </div>
       </div>
     </header>
