bed5f3c09e915eebef2bd84632eb994ed17c238e
Sabyrzhan Zhaslanuly <sabyrka.me@gmail.com>
2026-06-13 20:11:35 +0500

    feat: add base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Header } from './components/Header'
+import { Main } from './components/Main'
+import { Footer } from './components/Footer'
+
 export const App = () => {
   return (
-    <div>
-      <h1>Airbnb Clone</h1>
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   )
 }
